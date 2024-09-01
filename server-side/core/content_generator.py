e783b7ee02dbacef4938eea390345e6ca700f937
Vedant <kamblivedant50@gmail.com>
2024-09-01 20:49:14 +0530

    content_generator

--- server-side/core/content_generator.py
+++ server-side/core/content_generator.py
@@ -8,17 +8,7 @@ class ContentGenerator:
         self.openai_client = OpenAIProvider()
 
     def generate_content(self, output, module_name, api_key_to_use):
-        prompt_content_gen = """I'm seeking your expertise on the sub-module : {sub_module_name} \which comes under the module: {module_name}. As a knowledgeable educational assistant, I trust in your ability to provide a comprehensive explanation of this sub-module. Think about the sub-module step by step and design the best way to explain the sub-module to a student. Your response should cover essential aspects such as definition, in-depth examples, and any details crucial for understanding the topic. Please generate quality content on the sub-module ensuring the response is sufficiently detailed covering all the relevant topics related to the sub-module.
-
-    In your response, organize the information into subsections for clarity and elaborate on each subsection with suitable examples if and only if it is necessary. \
-    Include specific hypothetical scenario-based examples (only if it is necessary) or important sub-sections related to the subject to enhance practical understanding. \
-    If applicable, incorporate real-world examples, applications or use-cases to illustrate the relevance of the topic in various contexts. Additionally, incorporate anything \
-    that helps the student to better understand the topic. \
-    Ensure all the relevant aspects and topics related to the sub-module is covered in your response. \
-    Conclude your response by suggesting relevant URLs for further reading to empower users with additional resources on the subject. \
-    Please format your output as valid JSON, with the following keys: title_for_the_content (suitable title for the sub-module), \
-    content(an introduction of the sub-module), subsections (a list of dictionaries with keys - title and content), and urls (a list).
-    Be a good educational assistant and craft the best way to explain the sub-module.
+        prompt_content_gen = """I'm seeking your expertise on the sub-module : {sub_module_name} \which comes under the module: {module_name}. As a knowledgeable educational assistant, I trust in your ability to provide a comprehensive explanation of this sub-module. Think about the sub-module step by step and design the best way to explain the sub-module to a student. Your response should cover essential aspects such as definition, in-depth examples, and any details crucial for understanding the topic. Please generate quality content on the sub-module ensuring the response is sufficiently detailed covering all the relevant topics related to the sub-module. In your response, organize the information into subsections for clarity and elaborate on each subsection with suitable examples if and only if it is necessary. Include specific hypothetical scenario-based examples(only if it is necessary) or important sub-sections related to the subject to enhance practical understanding. If applicable, incorporate real-world examples, applications or use-cases to illustrate the relevance of the topic in various contexts. Additionally, incorporate anything that helps the student to better understand the topic. Ensure all the relevant aspects and topics related to the sub-module is covered in your response. Conclude your response by suggesting relevant URLs for further reading to empower users with additional resources on the subject. Please format your output as valid JSON, with the following keys: title_for_the_content (suitable title for the sub-module), content(an introduction of the sub-module), subsections (a list of dictionaries with keys - title and content), and urls (a list). Be a good educational assistant and craft the best way to explain the sub-module.
     """
         all_content = []
         flag = 1 if api_key_to_use== 'first' else (2 if api_key_to_use=='second' else 3 )
@@ -32,24 +22,12 @@ class ContentGenerator:
         return all_content
     
     def generate_content_from_web(self, sub_module_name, module_name, api_key_to_use):
-        content_generation_prompt = """I'm seeking your expertise on the subject of {sub_module_name} which comes under the module: {module_name}.\
-    As a knowledgeable educational assistant, I trust in your ability to provide \
-    a comprehensive explanation of this sub-module. Think about the sub-module step by step and design the best way to explain the sub-module to a student. \
-    Your response should cover essential aspects such as definition, in-depth examples, and any details crucial for understanding the topic. \
-    You have access to the subject's information which you have to use while generating the educational content. \
-    Please generate quality content on the sub-module ensuring the response is sufficiently detailed covering all the relevant topics related to the sub-module.
+        content_generation_prompt = """I'm seeking your expertise on the subject of {sub_module_name} which comes under the module: {module_name}. As a knowledgeable educational assistant, I trust in your ability to provide a comprehensive explanation of this sub-module. Think about the sub-module step by step and design the best way to explain the sub-module to a student. Your response should cover essential aspects such as definition, in-depth examples, and any details crucial for understanding the topic. You have access to the subject's information which you have to use while generating the educational content. Please generate quality content on the sub-module ensuring the response is sufficiently detailed covering all the relevant topics related to the sub-module.
 
     SUBJECT INFORMATION : ```{search_result}```
 
     --------------------------------
-    In your response, organize the information into subsections for clarity and elaborate on each subsection with suitable examples if and only if it is necessary. \
-    Include specific hypothetical scenario-based examples (only if it is necessary) or important sub-sections related to the subject to enhance practical understanding. \
-    If applicable, incorporate real-world examples, applications or use-cases to illustrate the relevance of the topic in various contexts. Additionally, incorporate anything \
-    that helps the student to better understand the topic. \
-    Ensure all the relevant aspects and topics related to the sub-module is covered in your response. \
-    Conclude your response by suggesting relevant URLs for further reading to empower users with additional resources on the subject. \
-    Please format your output as valid JSON, with the following keys: title_for_the_content (suitable title for the sub-module), \
-    content(an introduction of the sub-module), subsections (a list of dictionaries with keys - title and content), and urls (a list).
+    In your response, organize the information into subsections for clarity and elaborate on each subsection with suitable examples if and only if it is necessary. Include specific hypothetical scenario-based examples (only if it is necessary) or important sub-sections related to the subject to enhance practical understanding. If applicable, incorporate real-world examples, applications or use-cases to illustrate the relevance of the topic in various contexts. Additionally, incorporate anything that helps the student to better understand the topic. Ensure all the relevant aspects and topics related to the sub-module is covered in your response.Conclude your response by suggesting relevant URLs for further reading to empower users with additional resources on the subject.    Please format your output as valid JSON, with the following keys: title_for_the_content (suitable title for the sub-module), content(an introduction of the sub-module), subsections (a list of dictionaries with keys - title and content), and urls (a list).
     Be a good educational assistant and craft the best way to explain the sub-module.
         """
         flag = 1 if api_key_to_use== 'first' else (2 if api_key_to_use=='second' else 3 )
@@ -69,24 +47,14 @@ class ContentGenerator:
         return all_content
     
     def generate_content_from_textbook(self, module_name, output, profile, vectordb, api_key_to_use):
-        prompt= """I'm seeking your expertise on the subject of {sub_module_name} which comes under the module: {module_name}.\
-    As a knowledgeable educational assistant, I trust in your ability to provide \
-    a comprehensive explanation of this sub-module. Think about the sub-module step by step and design the best way to explain the sub-module to me. \
-    Your response should cover essential aspects such as definition, in-depth examples, and any details crucial for understanding the topic. \
-    You have access to the subject's information which you have to use while generating the educational content. \
-    Please generate quality content on the sub-module ensuring the response is sufficiently detailed covering all the relevant topics related to the sub-module. You will also \
-    be provided with my course requirements and needs. Structure the course according to my needs.
+        prompt= """I'm seeking your expertise on the subject of {sub_module_name} which comes under the module: {module_name}. As a knowledgeable educational assistant, I trust in your ability to provide a comprehensive explanation of this sub-module. Think about the sub-module step by step and design the best way to explain the sub-module to me. Your response should cover essential aspects such as definition, in-depth examples, and any details crucial for understanding the topic. You have access to the subject's information which you have to use while generating the educational content. Please generate quality content on the sub-module ensuring the response is sufficiently detailed covering all the relevant topics related to the sub-module. You will also be provided with my course requirements and needs. Structure the course according to my needs.
 
     MY COURSE REQUIREMENTS : {profile}
 
     SUBJECT INFORMATION : {context}
 
     --------------------------------
-    In your response, organize the information into subsections for clarity and elaborate on each subsection with suitable examples if and only if it is necessary. \
-    If applicable, incorporate real-world examples, applications or use-cases to illustrate the relevance of the topic in various contexts. Additionally, incorporate anything \
-    that helps the student to better understand the topic. \
-    Please format your output as valid JSON, with the following keys: title_for_the_content (suitable title for the sub-module), \
-    content(the main content of the sub-module), subsections (a list of dictionaries with keys - title and content).
+    In your response, organize the information into subsections for clarity and elaborate on each subsection with suitable examples if and only if it is necessary. If applicable, incorporate real-world examples, applications or use-cases to illustrate the relevance of the topic in various contexts. Additionally, incorporate anything that helps the student to better understand the topic. Please format your output as valid JSON, with the following keys: title_for_the_content (suitable title for the sub-module), content(the main content of the sub-module), subsections (a list of dictionaries with keys - title and content).
     Be a good educational assistant and craft the best way to explain the sub-module following my course requirement strictly..
     """
 
