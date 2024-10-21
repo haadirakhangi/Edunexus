38d1ba25727b8a9f600edbe46aa819940296a821
Vedant <kamblivedant50@gmail.com>
2024-10-21 23:03:47 +0530

    porcupine ka kaata chuba

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Landing";
 import CoursePage from "./pages/teacher/course/course";
 import CreatePage from "./pages/teacher/create";
+import Testing from "./pages/testing";
 function App() {
   return (
     <Routes>
       <Route element={<HomePage />} path="/" />
       <Route element={<CreatePage />} path="/teacher/create" />
       <Route element={<CoursePage />} path="/teacher/course" />
+      <Route element={<Testing />} path="/testing" />
     </Routes>
   );
 }
